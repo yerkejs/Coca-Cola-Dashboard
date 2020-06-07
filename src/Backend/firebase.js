368e14fead05450df14ff6675f9a87cba74f0855
Yelzhan Yerkebulan <33483071+030131550542@users.noreply.github.com>
2020-06-07 21:52:33 +0600

    ADD YOUR FIREBASE CONFIG

--- src/Backend/firebase.js
+++ src/Backend/firebase.js
@@ -3,14 +3,7 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBM0EU1C1QnY9k842njeyYY_5-CUsGkdaU",
-    authDomain: "coca-8cf9d.firebaseapp.com",
-    databaseURL: "https://coca-8cf9d.firebaseio.com",
-    projectId: "coca-8cf9d",
-    storageBucket: "coca-8cf9d.appspot.com",
-    messagingSenderId: "944579909497",
-    appId: "1:944579909497:web:766283c050a1fa896241a8",
-    measurementId: "G-W72M2MGLGD"
+    // YOUR CONFIG HERE
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth()
